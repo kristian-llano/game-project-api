b29ca58651ebc29599ec62c2ae35d904e61ad6f1
KLLANO23 <llano.kristian@gmail.com>
2021-06-03 18:44:01 -0400

    Migrations updated

--- api/models/character.py
+++ api/models/character.py
@@ -11,7 +11,7 @@ class Character(models.Model):
         RANGER = 'Ranger', ('Ranger')
 
     classes = models.CharField(
-        max_length=2,
+        max_length=20,
         choices=Classes.choices,
         default=Classes.KNIGHT,
     )
